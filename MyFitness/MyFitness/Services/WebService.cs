2ac560cef08aefd6dae5cdf0dd0d2986c6e72fd6
Aaron Day <aaronday@live.com.au>
2016-10-25 05:50:29 +1000

    Remaining Changes

--- MyFitness/MyFitness/Services/WebService.cs
+++ MyFitness/MyFitness/Services/WebService.cs
@@ -1,10 +1,7 @@
 ﻿using MyFitness.Model;
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Net;
-using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace MyFitness.Service
